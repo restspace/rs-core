f149b243bb770d60dfd723366047a5e8cc183dc5
DESKTOP-PTLE0C3\james <james@lynicon.com>
2022-08-03 19:59:48 +0100

    State api

--- ServiceContext.ts
+++ ServiceContext.ts
@@ -6,28 +6,37 @@ import { PipelineSpec } from "./PipelineSpec.ts";
 import { Url } from "./Url.ts";
 import * as log from "std/log/mod.ts";
 import { StateFunction } from "../rs-runtime/tenant.ts";
+import { Externality } from "../rs-runtime/pipeline/pipelineStep.ts";
 
 export interface SimpleServiceContext {
     tenant: string;
     prePost?: PrePost;
-    makeRequest: (msg: Message) => Promise<Message>;
+    makeRequest: (msg: Message, externality?: Externality) => Promise<Message>;
     runPipeline: (msg: Message, pipelineSpec: PipelineSpec, contextUrl?: Url, concurrencyLimit?: number) => Promise<Message>;
     logger: log.Logger;
     manifest: IServiceManifest;
     getAdapter: <T extends IAdapter>(url: string, config: unknown) => Promise<T>;
     makeProxyRequest?: (msg: Message) => Promise<Message>;
-    state?: StateFunction;
+    state: StateFunction;
 }
 
 export interface ServiceContext<TAdapter extends IAdapter> extends SimpleServiceContext {
     adapter: TAdapter;
 }
 
-export interface IStateClass<T> {
-    load(context: SimpleServiceContext, config: unknown): Promise<void>;
-    unload(): Promise<void>;
-}
+export class BaseStateClass {
+    load(_context: SimpleServiceContext, _config: unknown) {
+        return Promise.resolve();
+    }
+    unload() {
+        return Promise.resolve();
+    }
+} 
 
-export type StateClass<T extends IStateClass<T>> = new() => T;
+export type StateClass<T extends BaseStateClass> = new() => T;
 
 export type AdapterContext = Omit<SimpleServiceContext, "manifest">;
+
+export const nullState = <T>(_cons: new() => T) => {
+    throw new Error('State not set');
+}
