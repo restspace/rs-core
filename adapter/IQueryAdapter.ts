32bc6c8fda2a6ab3f079a7105426eaffa6f278ac
DESKTOP-PTLE0C3\james <james@lynicon.com>
2022-10-26 13:23:09 +0100

    Add skip/take to IQueryAdapter.runQuery

--- adapter/IQueryAdapter.ts
+++ adapter/IQueryAdapter.ts
@@ -1,6 +1,6 @@
 import { IAdapter } from "./IAdapter.ts";
 
 export interface IQueryAdapter extends IAdapter { 
-    runQuery: (query: string) => Promise<Record<string, unknown>[] | number>;
+    runQuery: (query: string, take?: number, skip?: number) => Promise<Record<string, unknown>[] | number>;
     quote: (obj: any) => string | Error;
 }
