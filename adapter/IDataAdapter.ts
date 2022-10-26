359e08370ab19a08e5c8350ef9cff9cbd402acd4
DESKTOP-PTLE0C3\james <james@lynicon.com>
2022-10-26 12:31:53 +0100

    add take, skip params to listDataset

--- adapter/IDataAdapter.ts
+++ adapter/IDataAdapter.ts
@@ -5,7 +5,7 @@ import { IAdapter } from "./IAdapter.ts";
 
 export interface IDataAdapter extends IAdapter { 
     readKey: (dataset: string, key: string) => Promise<Record<string, unknown> | number>;
-    listDataset: (dataset: string) => Promise<PathInfo[] | number>;
+    listDataset: (dataset: string, take?: number, skip?: number) => Promise<PathInfo[] | number>;
     writeKey: (dataset: string, key: string, data: MessageBody) => Promise<number>;
     deleteKey: (dataset: string, key: string) => Promise<number>;
     deleteDataset: (dataset: string) => Promise<number>;
