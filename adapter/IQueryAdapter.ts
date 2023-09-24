580158cc548b407becd1567095b718ce74014df0
restspace <james@restspace.io>
2023-09-24 16:08:00 +0100

    Add paging to IQueryAdapter

--- adapter/IQueryAdapter.ts
+++ adapter/IQueryAdapter.ts
@@ -1,6 +1,6 @@
 import { IAdapter } from "./IAdapter.ts";
 
 export interface IQueryAdapter extends IAdapter { 
-    runQuery: (query: string, take?: number, skip?: number) => Promise<Record<string, unknown>[] | number>;
-    quote: (obj: any) => string | Error;
+    runQuery: (query: string, variables: Record<string, unknown>, take?: number, skip?: number) => Promise<Record<string, unknown>[] | number>;
+    quote?: (obj: any) => string | Error;
 }
